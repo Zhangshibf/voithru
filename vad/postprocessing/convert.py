22fcc9e57a0eb32f9b750b65e6ce9ab9fe3949fa
张史小冰风 <zhangshibf@gmail.com>
2024-10-07 12:58:41 +0200

    Update convert.py

--- vad/postprocessing/convert.py
+++ vad/postprocessing/convert.py
@@ -4,12 +4,8 @@ import numpy as np
 
 
 def convert_frames_to_samples(frames, sample_rate=16000, hop_ms=10, window_ms=10):
-    try:
-        hop_samples = sample_rate * hop_ms / 1000
-    except:
-        print(sample_rate)
-        print(hop_ms)
-        print("aaa")
+
+    hop_samples = sample_rate * hop_ms / 1000
     window_samples = sample_rate * window_ms / 1000
 
     num_samples = int((len(frames) - 1) * hop_samples + window_samples)
